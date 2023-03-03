5d818f11e6242c584f3d2127e30f4c57925219a3
Linh2111 <120156683+Linh2111@users.noreply.github.com>
2023-03-02 19:54:23 -0800

    Complete Part 1

--- index.js
+++ index.js
@@ -34,5 +34,34 @@ else if(firstAnswer === 'right')
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`);
 
+    if(secondAnswer === 'past dragon')
+    {
+        let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. You only have a moment to respond, to stay or run:`);
+        
+        if(thirdAnswer === 'stay') 
+        {
+            let fourthAnswer = window.alert(`You and the dragon have an uplifting conversation about local politics and become lifelong friends.`);
+        }
+        else if(thirdAnswer === 'run') 
+        {
+            let fourthAnswer = window.alert(`Quickly, you run back to the cave's entrance. Sheepish, you return home.`);
+        }
+
+    }
+
+    else if(secondAnswer === 'away from dragon')
+    {
+        let thirdAnswer = window.prompt(`After walking a while longer, you come across a shiny blue flower. It is so beautiful that you decide you must either draw it or pick it. Which do you do?`);
+
+            if(thirdAnswer === 'draw it') 
+            {
+                let fourthAnswer = window.alert(`You draw the flower, capturing only a fraction of its beauty with your quill. You bring the drawing home, somewhat disappointed, but over time, discover joy in sharing it with your friends and family, recounting the story of your days as a sorcerer with the aid of the sketch.`);
+            }
+            else if(thirdAnswer === 'pick it') 
+            {
+                let fourthAnswer = window.alert(`You pick the flower and bring it home, and all marvel at its brilliance. However, over time it fades and eventually crumbles to dust.`);
+            }
+    }
+
 }
 
